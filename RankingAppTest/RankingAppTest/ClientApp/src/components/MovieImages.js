a0476a2667a6eb05558e93021c4a514ffac45837
ruairi670 <ruairi670@googlemail.com>
2023-07-28 12:10:17 -0400

    - RankingGrid added, RankingItems updated. - CSS updated

---
+++ RankingAppTest/RankingAppTest/ClientApp/src/components/MovieImages.js
@@ -0,0 +1,25 @@
+﻿import Godfather from '../Images/Movies/Godfather.png';
+import Highlander from '../Images/Movies/Highlander.png';
+import Highlander2 from '../Images/Movies/Highlander2.png';
+import LastOfTheMohicans from '../Images/Movies/LastOfTheMohicans.png';
+import PoliceAcademy6 from '../Images/Movies/PoliceAcademy6.png';
+import RearWindow from '../Images/Movies/RearWindow.png';
+import RoadHouse from '../Images/Movies/RoadHouse.png';
+import Shawshank from '../Images/Movies/Shawshank.png';
+import StarTreck4 from '../Images/Movies/StarTreck4.png';
+import Superman4 from '../Images/Movies/Superman4.png';
+
+const MovieImageArr = [
+    { id: 1, image: Godfather },
+    { id: 2, image: Highlander },
+    { id: 3, image: Highlander2 },
+    { id: 4, image: LastOfTheMohicans },
+    { id: 5, image: PoliceAcademy6 },
+    { id: 6, image: RearWindow },
+    { id: 7, image: RoadHouse },
+    { id: 8, image: Shawshank },
+    { id: 9, image: StarTreck4 },
+    { id: 10, image: Superman4 },
+];
+
+export default MovieImageArr;
--- RankingAppTest/RankingAppTest/ClientApp/src/components/RankItems.js
+++ RankingAppTest/RankingAppTest/ClientApp/src/components/RankItems.js
@@ -1,4 +1,6 @@
 ﻿import React, { useState, useEffect } from 'react';
+import MovieImageArr from './MovieImages.js';
+import RankingGrid from './RankingGrid.js';
 
 const RankItems = () => {
 
@@ -6,7 +8,7 @@ const RankItems = () => {
     const dataType = 1;
 
     useEffect(() => {
-        fetch(`item/${datatype}`)
+        fetch(`item/${dataType}`)
             .then((results) => {
                 return results.json();
             })
@@ -18,9 +20,18 @@ const RankItems = () => {
     return (
 
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} />
+            <div className="items-not-ranked">
             {
-                (items != null) ? items.map((item) => <h3>{item.title}</h3>):<div>Loading...</div>
-            }
+                    (items != null) ? items.map((item) =>
+                    <div className="unranked-cell">
+                        <img id={`item-${item.id}`} src={MovieImageArr.find(i => i.id === item.imageId)?.image} />
+                    </div>
+                    ): <div>Loading...</div>
+                }
+            </div>
         </main>
         )
 }
+
+export default RankItems;
