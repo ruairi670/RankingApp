bceb6028185d4b849469f4e3cb9d54e7bb390eb1
ruairi670 <ruairi670@googlemail.com>
2023-07-28 14:03:31 -0400

    Dragging and dropping functionality implemented

--- RankingAppTest/RankingAppTest/ClientApp/src/components/RankingGrid.js
+++ RankingAppTest/RankingAppTest/ClientApp/src/components/RankingGrid.js
@@ -1,6 +1,6 @@
 ﻿
 
-const RankingGrid = ({items, imgArr }) => {
+const RankingGrid = ({items, imgArr, drag, allowDrop, drop}) => {
 
     const rankingGrid = [];
     const cellCollectionTop = [];
@@ -10,8 +10,11 @@ const RankingGrid = ({items, imgArr }) => {
 
     function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) {
         if (rankNum > 0) {
-            var item = items.find(i => i.ranking === rankNum);
-            cellCollection.push(<div id={`rank-${rankNum}`} className="rank-cell"></div>);
+            var item = items.find(o => o.ranking === rankNum);
+            cellCollection.push(<div id={`rank-${rankNum}`} onDrop={drop} onDragOver={allowDrop} className="rank-cell">
+                {(item != null) ? <img id={`item-${item.id}`} src={imgArr.find(o => o.id === item.imageId)?.image} draggable="true" onDragStart={drag} />
+                    : null}
+            </div>);
         }
         else {
             cellCollection.push(<div className="row-label">
